5b85e703b651e94eeb0fe3428c0660abecfd558b
fred fauquette <frederic.fauquette.ffauq@axa.fr>
2020-11-24 08:53:06 +0100

    numeriquelles start

---
+++ Numeriquelles/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Frederic FAUQUETTE on 24/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
